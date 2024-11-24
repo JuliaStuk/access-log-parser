19d1843625622c2cf14e8d82b9b0f8dbf71ab271
JuliaStuk <51078301+JuliaStuk@users.noreply.github.com>
2024-11-24 22:43:05 +0300

    2. Синтаксис языка и Базовые типы данных

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайные числа от 0 дло 10: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: " + (number1+number2));
+        System.out.println("Разность чисел: " + (number1-number2));
+        System.out.println("Произведение чисел: " + (number1*number2));
+        System.out.println("Частное чисел: " + ((double)number1/number2));
+
     }
 }
