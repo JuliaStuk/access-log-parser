366a4bf0e4123a5b9b06d242cca6ecb19f093554
JuliaStuk <51078301+JuliaStuk@users.noreply.github.com>
2025-01-05 23:30:56 +0300

    задание по теме циклы

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
-import java.nio.channels.ScatteringByteChannel;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int N = 1;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExist || isDirectory) {
+                System.out.println("Путь к папке или несуществующему файлу");
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + N++);
+        }
     }
 }
