dae3de615bf0c80d1e7e04bc5ebd445abb5aad12
JuliaStuk <51078301+JuliaStuk@users.noreply.github.com>
2025-01-26 02:09:42 +0300

    Обработка исключений №1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,54 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int N = 1;
-        while (true) {
-            String path = new Scanner(System.in).nextLine();
-            File file = new File(path);
-            boolean fileExist = file.exists();
-            boolean isDirectory = file.isDirectory();
+        int lineCount = 0;
+        int maxLength = 0;
+        int minLength = Integer.MAX_VALUE;
+        String path = new Scanner(System.in).nextLine();
+        File file = new File(path);
+        boolean fileExist = file.exists();
+        boolean isDirectory = file.isDirectory();
 
-            if (!fileExist || isDirectory) {
-                System.out.println("Путь к папке или несуществующему файлу");
-                continue;
-            }
-            System.out.println("Путь указан верно");
-            System.out.println("Это файл номер " + N++);
+        if (!fileExist || isDirectory) {
+            System.out.println("Путь к папке или несуществующему файлу");
         }
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader =
+                    new BufferedReader(fileReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lineCount++;
+                int length = line.length();
+                if (length > maxLength) {
+                    maxLength = length;
+                }
+                if (length < minLength) {
+                    minLength = length;
+                }
+                if (length > 500) {
+                    throw new LineException("Строка превышает 1024 символа.");
+                }
+            }
+        } catch (LineException lx) {
+            lx.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("Общее количество строк в файле: " + lineCount);
+        System.out.println("Длина самой длинной строки: " + maxLength);
+        System.out.println("Длина самой короткой строки: " + minLength);
+    }
+}
+
+class LineException extends RuntimeException {
+    public LineException(String message) {
+        super(message);
     }
 }
